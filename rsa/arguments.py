ce110888f0181ad500a946d2a062a3b0a0161d67
bboose <beth.boose@medecision.com>
2021-04-17 13:50:59 -0400

    refactor encryption script, zhush up arguments

--- rsa/arguments.py
+++ rsa/arguments.py
@@ -10,8 +10,12 @@ class Arguments:
 
     @staticmethod
     def get_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("pprime", type=int, nargs="?", default=0, help="custom p (prime), has to be a prime int")
-        parser.add_argument("qprime", type=int, nargs="?", default=0, help="custom q (co prime), has to be an int that is coprime to pprime")
+        DESC_MSG = "Simple encryption/decryption script. Use the default primes or enter your own"
+        P_PRIME_MSG = "Optional prime (p), has to be a prime int"
+        Q_PRIME_MSG = "Optional co-prime to p (q), has to be an int that is coprime to p"
+
+        parser = argparse.ArgumentParser(description=DESC_MSG)
+        parser.add_argument("pprime", type=int, nargs="?", default=0, help=P_PRIME_MSG)
+        parser.add_argument("qprime", type=int, nargs="?", default=0, help=Q_PRIME_MSG)
         args = parser.parse_args()
         return args
--- rsa/encryption.py
+++ rsa/encryption.py
@@ -9,25 +9,29 @@ from arguments import Arguments
 from rsa_calc import RSACalc
 
 
-args = Arguments.get_arguments()
-
-raw_text = input("give me some text: ")
-if not raw_text.isprintable() or not raw_text:
-    sys.exit("you have to input numbers, letters or punctuation")
-
-# we're encrypting, so get the private key
-rsa = RSACalc(args.pprime, args.qprime)
-
-text_to_list = list(raw_text)
-
-print(f'List of Characters ={text_to_list}')
-
-public_key = rsa.public_key
+def main():
+    args = Arguments.get_arguments()
+    rsa = RSACalc(args.pprime, args.qprime)
+    user_text = get_input_from_user()
+    text_to_list = list(user_text)
+    print(f'List of Characters ={text_to_list}')
+    cipher = encrypt_input(text_to_list, rsa)
+    separator = " "
+    print(f'your cipher is: {separator.join(map(str, cipher))}')
 
 
-def encrypt_input(converted_text):
+def get_input_from_user():
+    user_text = input("give me some text: ")
+    if not user_text or not user_text.isprintable():
+        sys.exit("you have to input numbers, letters or punctuation")
+    return user_text
+
+
+def encrypt_input(converted_text, rsa):
     exp = rsa.exponent
+    public_key = rsa.public_key
     ciph = []
+
     for char in converted_text:
         ascii_value = rsa.ENCRYPT_DICTIONARY[char]
         # print(f'ascii:  {ascii_value}')
@@ -36,6 +40,5 @@ def encrypt_input(converted_text):
     return ciph
 
 
-cipher = encrypt_input(text_to_list)
-separator = " "
-print(f'your cipher is: {separator.join(map(str, cipher))}')
+if __name__ == "__main__":
+    main()
